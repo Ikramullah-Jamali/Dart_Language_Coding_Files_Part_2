540ba04ac81d6b24a649a23dcaf7c9938120a0a5
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-06-26 14:06:42 +0500

    Add files via upload

---
+++ Else_if_Ladder.dart
@@ -0,0 +1,23 @@
+
+void main()
+{
+  print("--- Else If Ladder---");
+
+  int marks=72;
+  if(marks>=80)
+    {
+      print("Result: A grade.");
+    }
+  else if(marks>=60)
+    {
+      print("Result: B grade.");
+    }
+  else if(marks>=50)
+   {
+    print("Result: C grade.");
+   }
+  else
+    {
+      print("Result: Fail.");
+    }
+}
