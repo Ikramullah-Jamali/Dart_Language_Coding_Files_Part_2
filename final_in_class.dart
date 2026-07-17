c8fd458a2f7c0ecd5a134e3350b5f9ab455508aa
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-07-17 17:06:21 +0500

    Add files via upload

---
+++ final_in_class.dart
@@ -0,0 +1,12 @@
+class Student
+{
+  final String name;
+  final int age;
+  Student(this.name,this.age);
+}
+void main()
+{
+  Student s1=Student("Shakeel",22);
+  print(s1.name);
+  print(s1.age);
+}
