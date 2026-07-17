94b4ebaccef5bae41efab9b984e2ecdfee1be1ea
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-07-17 17:06:00 +0500

    Add files via upload

---
+++ final_basics.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  final String name="Babar";
+  final int age=50;
+  final List<int> numbers=[1,2,3];
+  numbers.add(4); //allowed
+
+  print("Age: $age");
+  print("Name: $name");
+  print("List: $numbers");
+}
